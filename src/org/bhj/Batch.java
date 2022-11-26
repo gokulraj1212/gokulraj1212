2fd129ca9059bfcee74e742b4c6df1b7dce106fb
unknown <gokulraj.p1212@gmail.com>
2022-11-26 19:24:51 +0530

    gokulraj1212

--- src/org/bhj/Batch.java
+++ src/org/bhj/Batch.java
@@ -1,5 +1,15 @@
 package org.bhj;
 
 public class Batch {
+	private void empId() {
+		System.out.println("hi");
+
+	}
+	public static void main(String[] args) {
+		Batch h= new Batch();
+		h.empId();
+	}
+
+	
 
 }
